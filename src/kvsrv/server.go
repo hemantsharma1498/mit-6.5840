db2180d5a8a102412124a594e7ef3c811436066c
Hemant Sharma <hemantsharma1498@gmail.com>
2024-09-23 16:55:05 +0530

    Added basic assignment functionality

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,24 +14,38 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
-	mu sync.Mutex
+	mu     sync.Mutex
+	keyVal map[string]string
 
 	// Your definitions here.
 }
 
-
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	reply.Value = ""
+	if val, ok := kv.keyVal[args.Key]; ok {
+		reply.Value = val
+	}
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.keyVal[args.Key] = args.Value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	reply.Value = kv.keyVal[args.Key]
+	kv.keyVal[args.Key] = reply.Value + args.Value
 }
 
 func StartKVServer() *KVServer {
