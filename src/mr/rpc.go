7a67fddca9efe4b8536dc5050871736d93e36d8c
Hemant Sharma <hemantsharma1498@gmail.com>
2026-07-20 18:23:53 +0530

    feat: add ReduceTaskDone RPC types for reduce completion signaling

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -77,6 +77,13 @@ type GetReduceTaskRes struct {
 	Message           int
 }
 
+type ReduceTaskDoneReq struct {
+	ReduceTaskId int
+}
+
+type ReduceTaskDoneRes struct {
+}
+
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
 // Can't use the current directory since
